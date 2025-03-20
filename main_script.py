fb45994b773aad64acece8f0bca00c1228f5c30e
Mohamed kouider amar <kouideramar.mohamed@univ-medea.dz>
2025-03-20 21:11:06 +0100

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -2,6 +2,7 @@
 # ----------  Created by "Mohamed Kouider Amar"    ----------%
 # ----------  Email: kouideramar.mohamed@univ-medea.dz ------%
 # ---- Github: https://github.com/Kouideramar/GPTree.git ----%
+# -- Paper: https://doi.org/10.1016/j.compbiomed.2025.109988-%
 # -----------------------------------------------------------%
 
 """
